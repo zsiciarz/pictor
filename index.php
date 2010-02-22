b3c61fe7a5b98b74a12ff6a82b3fc2a599673e1f
Zbigniew Siciarz <antyqjon@interia.pl>
2010-02-22 15:57:08 +0100

    Added exception handling to index page.

--- index.php
+++ index.php
@@ -2,5 +2,13 @@
 error_reporting(E_ALL);
 
 require_once 'src/Pictor/Image.php';
-$img = new \Pictor\Image();
-var_dump($img);
+
+try
+{
+    $img = new \Pictor\Image();
+    var_dump($img);
+}
+catch (\Pictor\Exception $e)
+{
+    die($e->getMessage());
+}
