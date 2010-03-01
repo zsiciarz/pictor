124ba36731d76aefcad27db29189850cd531a217
Zbigniew Siciarz <antyqjon@interia.pl>
2010-03-01 16:39:34 +0100

    Updated font example.

--- font_test.php
+++ font_test.php
@@ -9,11 +9,11 @@ use Pictor\Point as Point, Pictor\Size as Size;
 
 try
 {
-    $font = new \Pictor\Font('C:\\Windows\\fonts\\arial.ttf', 16);
+    $arial = new \Pictor\Font('C:\\Windows\\fonts\\arial.ttf', 16);
     $verdana = new \Pictor\Font('C:\\Windows\\fonts\\verdana.ttf', 20);
     $img = new \Pictor\Image(new Size(400, 400));
-    $img->drawText($img->getCenter(), 'LENA', $font, 'F00')
-        ->drawText(new Point(50, 50), 'Verdana', $verdana, '040', -25)
+    $img->drawText($img->getCenter(), 'LENA', $arial, 'F00')
+        ->drawText(new Point(50, 50), 'Verdana', $verdana, '070', -25)
         ->show();
 }
 catch (\Pictor\Exception $e)
