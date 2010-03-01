e0155a9a8255873b852063da6a1e7dca1a1fc7c9
Zbigniew Siciarz <antyqjon@interia.pl>
2010-03-01 19:42:50 +0100

    Prepared for adding image plugins.

---
+++ plugin_test.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+
+require_once 'src/Pictor/Loader.php';
+$loader = new \Pictor\Loader();
+$loader->register();
+
+use \Pictor\Point as Point, \Pictor\Size as Size;
+
+try
+{
+    $img = new \Pictor\Image(new Size(600, 600));
+    $img->drawRectangle(new Point(0, 0), $img->getSize(), '#EEE', true)
+        ->show();
+}
+catch (\Pictor\Exception $e)
+{
+    die($e->getMessage());
+}
