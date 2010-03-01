1a06780a66c55aa3a9536012240fe34866e78184
Zbigniew Siciarz <antyqjon@interia.pl>
2010-03-01 16:19:41 +0100

    Updated the font example.

--- font_test.php
+++ font_test.php
@@ -10,9 +10,11 @@ use Pictor\Point as Point, Pictor\Size as Size;
 try
 {
     $font = new \Pictor\Font('C:\\Windows\\fonts\\arial.ttf', 16);
+    $verdana = new \Pictor\Font('C:\\Windows\\fonts\\verdana.ttf', 20);
     $img = new \Pictor\Image();
     $img->load('img/lena.png')
         ->drawText($img->getCenter(), 'LENA', $font, 'F00')
+        ->drawText(new Point(50, 50), 'Verdana', $verdana, '040', -25)
         ->show();
 }
 catch (\Pictor\Exception $e)
