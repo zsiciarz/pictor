4b8a65684c1743d735420667d181d204fdd68490
Zbigniew Siciarz <antyqjon@interia.pl>
2010-03-02 15:32:07 +0100

    Updated the selection example.

--- select_test.php
+++ select_test.php
@@ -17,6 +17,12 @@ try
     $img->select(new Point(0, 0), $selectionSize)
         ->drawRectangle(new Point(0, 0), $selectionSize->adjust(-1, -1), '#0F0')
         ->deselect()
+        ->select(new Point (100, 120), new Size(100, 30))
+        ->invert()
+        ->deselect()
+        ->select(new Point(0, 200), new Size(50, 50))
+        ->grid(4, 4, '#FFF') // plugins can work on selections too
+        ->deselect()
         ->show();
 }
 catch (\Pictor\Exception $e)
