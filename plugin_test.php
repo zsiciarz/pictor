bb04899afa986192b17a4b45647df682da417b6c
Zbigniew Siciarz <antyqjon@interia.pl>
2010-03-03 11:18:15 +0100

    Fixed the plugin test to draw grid INSIDE the margin, and not overlap.

--- plugin_test.php
+++ plugin_test.php
@@ -11,7 +11,9 @@ try
 {
     $img = new \Pictor\Image(new Size(600, 300));
     $img->drawRectangle(new Point(0, 0), $img->getSize(), '#EEE', true)
+        ->select(new Point(20, 20), $img->getSize()->adjust(-40, -40))
         ->grid(5, 5, '#666') // from plugin GridPlugin
+        ->deselect()
         ->border(20, '#399') // BorderPlugin
         ->show();
 }
